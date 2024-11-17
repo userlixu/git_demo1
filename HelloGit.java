c7f0e4f22286eaef8112ea47d940a457052e4996
userli <userli@126.com>
2024-11-17 16:27:38 +0800

    睡觉功能

--- HelloGit.java
+++ HelloGit.java
@@ -3,8 +3,12 @@ public class HelloGit
 	public static void main （String[] args）{
 		System.out.println("hello git");
 		Lean();
+		Sleep();
 	}
 	public void Lean(){
 		System.out.println("学习");
+	}
+	public void Sleep(){
+		System.out.println("睡觉");
 	}
 }	
